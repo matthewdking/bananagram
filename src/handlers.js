c07fad4ee23633bc259bd4fcbe5abc6a26cf90f9
matthewdking <matthew.d.king@outlook.com>
2017-07-29 13:32:41 +0300

    all source files fixed add public route relates #12

--- src/handlers.js
+++ src/handlers.js
@@ -6,7 +6,7 @@ var headers = {
   'content-type' : 'text/html'
 }
 
-const handleHome = (req, res) => {
+const handleHome = (res) => {
   const filePath = path.join(__dirname, "..", "public", "index.html");
   fs.readFile(filePath, (err, file) => {
     if (err) {
@@ -19,6 +19,28 @@ const handleHome = (req, res) => {
   });
 }
 
+const handlePublic = (res, url) => { //declare handlePublic as a function
+  const filePath = path.join(__dirname, '..', url); // declare filePath as path to file
+  const extension = url.split('.')[1]; //splt url on the dot to work out file type eg html, jpg, css
+  const extensionType = { //array of extensions as keys and values
+    html: 'text/html',
+    css: 'text/css',
+    js: 'application/javascript',
+    ico: 'image/x-icon'
+  }
+  fs.readFile(filePath, (err, file) => { //read file with err first callback
+    if (err) { //if err do this
+      console.log(err); // log err
+      res.writeHead(500, 'Content-Type: text/html'); // err code 500
+      res.end('<h1>Sorry Mario broke the server</h1>'); // end connection and send this html
+    }
+    else { //else do this
+      res.writeHead(200, `Content-Type: ${extensionType[extension]}`); // code 200 with content type found using extension type object
+      res.end(file); // end connection and send file
+    }
+  })
+}
+
 const notFound = (req, res) => {
   res.writeHead(404, headers);
   res.end('We can\'t find what you\'re looking for');
@@ -26,5 +48,6 @@ const notFound = (req, res) => {
 
 module.exports = {
   handleHome,
+  handlePublic,
   notFound
 }
